e01db65c0b2f5608e84ce278618ceac5ff3e7a32
ashareef <azher.m.shareef@gmail.com>
2018-07-14 01:12:00 -0400

    spring init

---
+++ src/main/java/io/shareef/azher/bracken/BrackenApplication.java
@@ -0,0 +1,12 @@
+package io.shareef.azher.bracken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrackenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrackenApplication.class, args);
+	}
+}
